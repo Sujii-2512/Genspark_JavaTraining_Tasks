637a351fffb6f60dd46b9b6c49eb56c6172728b8
SUJITHA R <91478382+Sujii-2512@users.noreply.github.com>
2025-05-19 19:37:17 +0530

    Update Solution.java

--- src/Task3/RemoveLinkedListElements/Solution.java
+++ src/Task3/RemoveLinkedListElements/Solution.java
@@ -1,4 +1,27 @@
-package Task3.RemoveLinkedListElements;
+class Solution {
+  public ListNode removeElements(ListNode head, int val) {
+         // Handle the case when the list is empty
+        if (head == null) return null;
 
-public class Solution {
+        ListNode previous = null;
+        ListNode current = head;
+
+        while (current != null) {
+            if (current.val == val) {
+                if (previous == null) {
+                    // Removing the head node
+                    head = current.next;
+                    current = head;
+                } else {
+                    previous.next = current.next;
+                    current = current.next;
+                }
+            } else {
+                // Move both pointers forward
+                previous = current;
+                current = current.next;
+            }
+        }
+        return head;
+    }
 }
