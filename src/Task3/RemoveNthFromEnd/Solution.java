88b144dbcf257c54ef29442e02f9b13414ca2eaf
SUJITHA R <91478382+Sujii-2512@users.noreply.github.com>
2025-05-19 19:37:59 +0530

    Update Solution.java

--- src/Task3/RemoveNthFromEnd/Solution.java
+++ src/Task3/RemoveNthFromEnd/Solution.java
@@ -1,4 +1,37 @@
-package Task3.RemoveNthFromEnd;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode lead = head;
+        ListNode follow = head;
 
-public class Solution {
+        // Move lead pointer n steps ahead
+        for (int i = 0; i < n; i++) {
+            if (lead == null) return head;  // n is larger than list length, just return head
+            lead = lead.next;
+        }
+
+        // If lead is null after moving n steps remove head
+        if (lead == null) {
+            return head.next;
+        }
+
+        // Move both pointers until lead reaches the last node
+        while (lead.next != null) {
+            lead = lead.next;
+            follow = follow.next;
+        }
+
+        follow.next = follow.next.next;
+
+        return head; 
+    }
 }
